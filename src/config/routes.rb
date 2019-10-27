de9cfcfb39c0b83c7c128ed649e186d19a9ede04
LevPewPew <levtoth1988@gmail.com>
2019-10-28 10:05:48 +1100

    testing new repo with .gitignore fixed and all confidential info now not pushed

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   get "/", to: "pages#home", as: "root"
+
   resources :listings
-  get "user_settings", to: "pages#user_settings", as: "user_settings"
   get "my_listings", to: "listings#my_listings", as: "my_listings"
+
+  get "user_settings", to: "pages#user_settings", as: "user_settings"
 end
