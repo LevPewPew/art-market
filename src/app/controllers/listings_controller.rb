b460557712ce433abd177658c5e4195a852734d9
LevPewPew <levtoth1988@gmail.com>
2019-11-04 10:06:33 +1100

    moved stripe session code from controller to a helper

--- src/app/controllers/listings_controller.rb
+++ src/app/controllers/listings_controller.rb
@@ -50,25 +50,7 @@ class ListingsController < ApplicationController
     end
 
     if !current_user.nil?
-      session = Stripe::Checkout::Session.create(
-        payment_method_types: ['card'],
-        customer_email: current_user.email,
-        line_items: [{
-          name: @listing.title,
-          description: @listing.description,
-          amount: @listing.price * 100,
-          currency: 'aud',
-          quantity: 1
-        }],
-        payment_intent_data: {
-          metadata: {
-            user_id: current_user.id,
-            listing_id: @listing.id
-          }
-        },
-        success_url: "#{root_url}payments/success?userId=#{current_user.id}&listingId=#{@listing.id}",
-        cancel_url: "#{root_url}listings"
-      )
+      session = helpers.stripe_session
     
       @session_id = session.id
     end
--- src/app/helpers/listings_helper.rb
+++ src/app/helpers/listings_helper.rb
@@ -1,2 +1,25 @@
 module ListingsHelper
+  def stripe_session
+    session = Stripe::Checkout::Session.create(
+      payment_method_types: ['card'],
+      customer_email: current_user.email,
+      line_items: [{
+        name: @listing.title,
+        description: @listing.description,
+        amount: @listing.price * 100,
+        currency: 'aud',
+        quantity: 1
+      }],
+      payment_intent_data: {
+        metadata: {
+          user_id: current_user.id,
+          listing_id: @listing.id
+        }
+      },
+      success_url: "#{root_url}payments/success?userId=#{current_user.id}&listingId=#{@listing.id}",
+      cancel_url: "#{root_url}listings"
+    )
+
+    return session
+  end
 end
