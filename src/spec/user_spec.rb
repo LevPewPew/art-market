dc0c8c39394e27361961d89b9e3edd77925020cb
LevPewPew <levtoth1988@gmail.com>
2019-11-04 11:41:15 +1100

    heroku test

--- src/spec/user_spec.rb
+++ src/spec/user_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe 'Home Page', type: :feature do
-  scenario 'index page' do
-    visit root_path
-    expect(page).to have_content('Art Haus')
+RSpec.feature "Logs In" do
+  let!(:project) { FactoryBot.create(:project) }
+
+  it "updates a project successfully" do
+    visit "/"
+    click_link project.name
+    click_link "Edit"
+    fill_in "Name", with: "Ticketee"
+    click_button "Update Project"
+    expect(page).to have_content("Project was updated successfully")
   end
 end
